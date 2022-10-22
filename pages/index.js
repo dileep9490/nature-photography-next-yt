38bbe298094ece352d8ad4919fa28a9da8e16f9d
dileep <dileepgudimetla9490@gmail.com>
2022-10-22 14:18:28 +0530

    added slider component

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
 import Head from "next/head";
 import Image from "next/image";
 import Hero from "../components/Hero";
-
+import Slider from "../components/Slider";
+import {SliderData} from "../components/SliderData"
 
 export default function Home() {
 	return (
@@ -12,6 +13,7 @@ export default function Home() {
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
       <Hero heading="Captur Photography" message="I capture moments in nature and keep them alive."/>
+	  <Slider slides={SliderData}/>
 		</div>
 	);
 }
